f306ae5f9e10298e50f14682a4e3b75e9a8da9b8
naimrb <naimbarka@hotmail.com>
2024-02-29 08:02:14 +0000

    Added backend, user functionalities (login, signup and logout)

---
+++ api/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, Response, session
+from lib.user_repository import UserRepository
+from lib.user import User
+from lib.db import get_flask_database_connection
+
+import json
+import secrets
+
+app = Flask(__name__)
+app.secret_key = b'secretkey'
+
+@app.route('/user/login', methods=['POST'])
+def user_login():
+    if 'token' in session:
+        response = {'token': session['token'], 'message': 'Already logged in'}
+        return Response(json.dumps(response), status=200, mimetype='application/json') 
+    
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        
+        connection = get_flask_database_connection(app)
+        user_repo = UserRepository(connection)
+        result = user_repo.find(username)
+        
+        if result != None and password == result.password:
+            token = secrets.token_urlsafe(64)
+            session['token'] = token        
+            response = {'id': result.id, 'username': result.username, 'email': result.email, 'token': token, 'message': 'Succesfully logged In'}
+            return Response(json.dumps(response), status=200, mimetype='application/json')
+        
+        else:
+            return Response(response={}, status=400, mimetype='application/json')
+
+@app.route('/user/signup', methods=['POST'])
+def user_signup():
+    if 'token' in session:
+        response = {'token': session['token'], 'message': 'Already logged in'}
+        return Response(json.dumps(response), status=200, mimetype='application/json') 
+    
+    else: 
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        
+        connection = get_flask_database_connection(app)
+        user_repo = UserRepository(connection)
+        result = user_repo.find(username, email)
+        
+        if result != None:
+            response = {'message': 'User or email already exists'}
+            return Response(json.dumps(response), status=400, mimetype='application/json')
+        
+        else:
+            user = User(None, username, password, email)
+            user_repo.add(user)
+            
+            response = {'message': 'Account sucessfully created'}
+            return Response(json.dumps(response), status=200, mimetype='application/json')
+         
+        
+    
+@app.route('/user/logout', methods=['GET'])
+def user_logout():
+    session.pop('token', None)
+    return Response(status=200) 
+        
+
+#if 'token' in session
---
+++ api/lib/db.py
@@ -0,0 +1,29 @@
+import psycopg2
+from flask import g
+
+DB_NAME = 'daily_grind'
+DB_USER = 'naim'
+
+class DBConnection:
+    
+    def connect(self):
+        try:
+            self.connection = psycopg2.connect(f'dbname={DB_NAME} user={DB_USER}')
+        except psycopg2.OperationalError:
+            raise Exception('Connection Error')
+    
+    def execute(self, query, params=[]):
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, params)
+            if cursor.description != None:
+                result = cursor.fetchall()
+            else:
+                result = None
+            self.connection.commit()
+            return result
+                
+def get_flask_database_connection(app):
+    if not hasattr(g, 'flask_database_connection'):
+        g.flask_database_connection = DBConnection()
+        g.flask_database_connection.connect()
+    return g.flask_database_connection
