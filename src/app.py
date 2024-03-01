c75bb4f8c609989a3ba5b98f647e403b34edc150
naimrb <naimbarka@hotmail.com>
2024-03-01 09:57:57 +0000

    Added signup screen, fixed error_label not showing when switching screen

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from screen.login import LoginScreen
 from screen.main import MainScreen
+from screen.signup import SignupScreen
 
 Login = LoginScreen(1000, 650)
 Main = MainScreen(1000, 650)
+Signup = SignupScreen(1000, 650) 
 
 currentScreen = Login
 
@@ -14,5 +16,11 @@ while run:
         Main.data = currentScreen.data
         currentScreen = Main
     
+    elif response == 'signup':
+        currentScreen = Signup
+    
+    elif response == 'login':
+        currentScreen = Login
+        
     elif response == 'stop':
         run = False
