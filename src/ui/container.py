fd173af0dd43b39d0af51f755e0d40c49214b70c
naimrb <naimbarka@hotmail.com>
2024-03-07 16:21:23 +0000

    Updated main screen, added buttons

--- src/ui/container.py
+++ src/ui/container.py
@@ -2,7 +2,7 @@ import pygame, math
 
 from pygame_gui.core import ObjectID
 from pygame_gui.core import UIContainer
-from pygame_gui.elements import UILabel
+from pygame_gui.elements import UILabel, UIButton
 
 class Container:
     def __init__(self, x, y, width, height, manager, window_surface, padding_x=0, padding_y=0):
@@ -20,6 +20,7 @@ class Container:
         self.padding_y = padding_y
         self.padding_rect =  pygame.Rect((self.x + padding_x / 2, self.y + padding_y / 2), (self.width - padding_x, self.height - padding_y))
         self.elements = []
+        self.buttons = []
 
     def add_label(self, text, relative_rect:pygame.Rect, style=None, fill=None):
         relative_rect[0] += self.padding_x / 2
@@ -40,7 +41,19 @@ class Container:
                 'rect': relative_rect
             }
             self.elements.append(element)
-    
+
+    def add_button(self, text, relative_rect:pygame.Rect, style=None):
+        relative_rect[0] += self.padding_x / 2
+        relative_rect[1] += self.padding_y / 2
+
+        if style!= None:
+            object_id=ObjectID(class_id='@test', object_id=f'#{style}')
+        else:
+            object_id=None
+
+        self.buttons.append(UIButton(relative_rect, text=text, manager=self.manager, container=self.container, object_id=object_id))
+
+
     def fill(self, color):
         element = {
             'color': color,
@@ -55,11 +68,11 @@ class Container:
             else:
                 pygame.draw.rect(self.window_surface, element['color'], element['rect'])
     
-    def add_bar(self, level, value, color, relative_rect:pygame.Rect):
+    def add_bar(self, level, value, color, relative_rect:pygame.Rect, xp_next_level=20):
         max_value = 100
         if level > 1:
             for i in range(1, level):
-                max_value += 20
+                max_value += xp_next_level
         
         rect_top = relative_rect[:]
         rect_top[0] += self.padding_x / 2 + self.x
@@ -73,7 +86,7 @@ class Container:
         self.elements.append(element_top)
 
         rect_back = rect_top[:]
-        min_value = max_value - 20
+        min_value = max_value - xp_next_level
         min_max_diff = max_value - min_value
         max_pixel = rect_top[2]
 
