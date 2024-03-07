8eb6df18ecd63d08c34637d71e1c542f9caeefa1
naimrb <naimbarka@hotmail.com>
2024-03-07 13:59:36 +0000

    Added container bar element

--- src/screen/main.py
+++ src/screen/main.py
@@ -30,15 +30,39 @@ class MainScreen(Screen):
         clock = pygame.time.Clock()
         is_running = True
 
-        container_user = Container(0, 0, self.width, 80, manager, window_surface)
+        user_stats = {
+            'user_level': 3,
+            'strength_level': 5,
+            'strength_experience': 180,
+            'intellect_level': 5,
+            'intellect_experience': 180,
+            'user_money': 50
+        }
 
-        container_stats = Container(0, container_user.height, self.width/2, 350, manager, window_surface, padding=50)
-        container_history = Container(container_stats.width, container_user.height, self.width/2, 350, manager, window_surface, padding=50)
-        
-        container_minigames = Container(0, container_stats.y + container_stats.height, self.width, self.height - container_stats.y, manager, window_surface)
+        container_user = Container(0, 20, self.width, 50, manager, window_surface, padding_x=50)
+        container_user.add_label(f"LVL {user_stats['user_level']}", pygame.Rect((0,0), (100, 50)), 'user_stat', '#ffd105')
+        container_user.add_label(f"Welcome {self.data['user'].username}, have fun!", pygame.Rect((175, 0), (600, 50)), 'user_stat', '#ffd105')
+        container_user.add_label(f"${user_stats['user_money']}", pygame.Rect((container_user.padding_rect[2] - 100, 0), (100, 50)), 'user_stat', '#ffd105')
 
+        container_stats = Container(0, container_user.height + container_user.y, self.width/2, 350, manager, window_surface, padding_x=50, padding_y=50)
+        container_stats.fill('#b617ff')
+        container_stats.add_label('Stats', pygame.Rect((0, 10), (container_stats.padding_rect[2], 40)), 'title')
         
-        container_history.add_label('History', pygame.Rect((0, 10), (container_history.width - container_history.padding, 40)), 'title')
+        user_lvl_bar_rect = pygame.Rect((container_stats.padding_rect[2]/2-175, container_stats.padding_rect[3]-25 - 110), (350, 25))
+        container_stats.add_bar(5, 170, '#0307fc', user_lvl_bar_rect)
+        container_stats.add_label(f"Level 5", user_lvl_bar_rect, 'label_bar')
+
+        intelect_lvl_bar_rect = pygame.Rect((container_stats.padding_rect[2]/2-175, container_stats.padding_rect[3]-25 - 80), (350, 25))
+        container_stats.add_bar(5, 165, '#09e343', intelect_lvl_bar_rect)
+        container_stats.add_label(f"Level 5", intelect_lvl_bar_rect, 'label_bar')
+ 
+        strength_lvl_bar_rect = pygame.Rect((container_stats.padding_rect[2]/2-175, container_stats.padding_rect[3]-25 - 50), (350, 25))
+        container_stats.add_bar(5, 180, '#fc0303', strength_lvl_bar_rect)
+        container_stats.add_label(f"Level 5", strength_lvl_bar_rect, 'label_bar')
+        
+        container_history = Container(container_stats.width, container_user.height + container_user.y, self.width/2, 350, manager, window_surface, padding_x=50, padding_y=50)        
+        container_history.fill('#42adff')
+        container_history.add_label('History', pygame.Rect((0, 10), (container_history.padding_rect[2], 40)), 'title')
         stats_user = get_stats(self.data['user'].id, self.data['session'])
         if stats_user:
             i = 0
@@ -48,7 +72,11 @@ class MainScreen(Screen):
 
             for stat in stats:
                 i += 1 
-                container_history.add_label(f"{stat['game']} - {stat['score']} points", pygame.Rect((0, 40 + 35 * i), (container_history.width - container_history.padding, 30)), 'content_center')
+                container_history.add_label(f"{stat['game']} - {stat['score']} points ({stat['experience']}XP ${stat['money']})", pygame.Rect((0, 40 + 35 * i), (container_history.width - container_history.padding_x, 25)), 'content_center')
+
+
+        container_minigames = Container(0, container_stats.y + container_stats.height, self.width, self.height - container_stats.y, manager, window_surface)
+        container_minigames.fill('#ffffff')
 
         while is_running:
             time_delta = clock.tick(60)/1000.0
@@ -63,10 +91,9 @@ class MainScreen(Screen):
 
             window_surface.blit(background, (0, 0))
 
-            container_user.fill('#012abc')
-            container_stats.fill('#b617ff')
-            container_history.fill('#42adff')
-            container_minigames.fill('#ffffff')
+            container_history.display()
+            container_stats.display()
+            container_user.display()    
 
             manager.draw_ui(window_surface)
 
